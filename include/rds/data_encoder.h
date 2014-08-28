7d174397c0518855aee4a02a736842916415c094
Bastian Bloessl <bloessl@ccs-labs.org>
2014-08-28 08:32:41 +0200

    encoder: do not use XML

--- include/rds/data_encoder.h
+++ include/rds/data_encoder.h
@@ -30,10 +30,6 @@
 #include <string.h>
 #include <vector>
 #include <iostream>
-/* adds dependency: libxml2-dev */
-#include <libxml/parser.h>
-#include <libxml/tree.h>
-#include <libxml/xmlmemory.h>
 
 namespace gr {
 namespace rds {
@@ -65,7 +61,6 @@ private:
 	int event;
 	int location;
 	gr::thread::mutex d_mutex;
-	char *xml;
 
 /* each type 0 group contains 2 out of 8 PS characters;
  * this is used to count 0..3 and send all PS characters */
@@ -79,18 +74,12 @@ private:
 	int d_current_buffer;
 /* loops through the buffer, pushing out the symbols */
 	int d_buffer_bit_counter;
-/* 0..16+A/B = 32 groups. 0A is groups[0], 0B is groups[16]. use %16.
- * ==0 means group not present, ==1 means group present */
-	int ngroups;
 	int groups[32];
 /* nbuffers might be != ngroups, e.g. group 0A needs 4 buffers */
 	int nbuffers;
 
 // Functions
-	data_encoder (const char*);	// private constructor
-	int read_xml(const char*);
-	void print_element_names(xmlNode*);
-	void assign_from_xml(const char*, const char*, const int);
+	data_encoder ();  // private constructor
 	void reset_rds_data();
 	void count_groups();
 	void create_group(const int, const bool);
@@ -102,12 +91,13 @@ private:
 	unsigned int encode_af(double);
 	unsigned int calc_syndrome(unsigned long, unsigned char);
 	void rds_in(pmt::pmt_t msg);
-	void init(char *txt=NULL, int len=0);
+	void init(char *txt = NULL, int len = 0);
+	void assign_value(const char *field, const char *value);
 
 public:
 	typedef boost::shared_ptr<data_encoder> sptr;
-	static sptr make(const char*);
-	~data_encoder();		// public destructor
+	static sptr make();
+	~data_encoder();  // public destructor
 	int work (int noutput_items,
 			gr_vector_const_void_star &input_items,
 			gr_vector_void_star &output_items);
